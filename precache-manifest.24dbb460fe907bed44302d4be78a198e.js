aa2c12dc7a23d7e15563dd6f0e07521d076acca1
Gnuck <ntm32@cornell.edu>
2019-09-21 04:38:57 -0700

    Updates

---
+++ precache-manifest.24dbb460fe907bed44302d4be78a198e.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a45b0f97d9126b7531fe5d0b9a17b00f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "531fc544d3a7e714aa4c",
+    "url": "/static/css/main.f2baa2db.chunk.css"
+  },
+  {
+    "revision": "17e39880963cb3cb1b08",
+    "url": "/static/js/2.4d0c177f.chunk.js"
+  },
+  {
+    "revision": "531fc544d3a7e714aa4c",
+    "url": "/static/js/main.fb197050.chunk.js"
+  },
+  {
+    "revision": "5d3de7f4d38be66d413a",
+    "url": "/static/js/runtime-main.5559a321.js"
+  },
+  {
+    "revision": "141dbd81d55dbff48aa95a0fcac60c11",
+    "url": "/static/media/DisposableDroidBB_bld.141dbd81.otf"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/static/media/logo.25bf045c.svg"
+  },
+  {
+    "revision": "cf8cc261a3619d667bde4bf122eba7fd",
+    "url": "/static/media/titleImage.cf8cc261.png"
+  }
+]);
